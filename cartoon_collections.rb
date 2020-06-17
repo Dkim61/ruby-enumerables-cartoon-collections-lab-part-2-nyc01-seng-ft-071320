6fb857c46dabe69a229625d1d38e57155478c288
david <dkim187@binghamton.edu>
2020-06-17 23:36:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find { |element| valid_calls.includelearn?(element)}
+  planeteer_calls.find { |element| valid_calls.include?(element)}
 end
